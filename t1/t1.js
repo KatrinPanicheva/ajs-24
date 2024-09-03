45cc16303dab59b44729595fbaf5c692e3ecb9e2
KatrinPanicheva <katrin.panicheva@hotmail.com>
2024-09-03 15:09:47 +0300

    uus

---
+++ t1/t1.js
@@ -0,0 +1,60 @@
+'use strict';
+import { restaurantModal, restaurantRow } from './components.js';
+import {fetchData} from './fetchData.js';
+import { apiURL } from './variables.js';
+
+const kohde = document.querySelector('tbody');
+const modaali = document.querySelector('dialog');
+const info = document.querySelector('#info');
+const closeModal = document.querySelector('#close-modal');
+
+closeModal.addEventListener('click', () => {
+  modaali.close();
+});
+
+
+const teeRavintolaLista = async () => {
+  const restaurants = await fetchData(apiURL + '/api/v1/restaurants');
+
+  // your code here
+
+  //aakkosjärjestys
+  restaurants.sort((a, b) => a.name.localeCompare(b.name));
+
+  //ravintolat käydään läpi
+  for (const restaurant of restaurants) {
+    if (restaurant) {
+      const {_id} = restaurant;
+
+      //ravintola rivin html rivi
+      const rivi = restaurantRow(restaurant);
+
+      rivi.addEventListener('click', async () => {
+        const korostetut = document.querySelectorAll('.highlight');
+        for (const korostettu of korostetut) {
+          korostettu.classList.remove('highlight');
+        }
+
+        rivi.classList.add('highlight');
+        // hae päivän ruokalista
+        const paivanLista = await fetchData(
+          apiURL + `/api/v1/restaurants/daily/${_id}/fi`
+        );
+
+        console.log('päivan lista', paivanLista.courses);
+        //tulosta päivän lista
+        info.innerHTML = '';
+        const ravintolaHTML = restaurantModal(restaurant, paivanLista.courses);
+        info.insertAdjacentHTML('beforeend', ravintolaHTML);
+
+        modaali.showModal();
+
+
+      });
+
+      kohde.append(rivi);
+    }
+  }
+};
+
+teeRavintolaLista();
